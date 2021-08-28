7252ffb11243929a0a979339d02f5fd99454b4da
mugon-dev <jmg41490400@gmail.com>
2021-08-28 18:11:38 +0900

    style: add appbar style

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:carrot_market_ui/theme.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,8 +7,34 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('HomeScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            const Text("좌동"),
+            const SizedBox(width: 4.0),
+            const Icon(
+              CupertinoIcons.chevron_down,
+              size: 15.0,
+            )
+          ],
+        ),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(CupertinoIcons.search)),
+          IconButton(
+              onPressed: () {}, icon: const Icon(CupertinoIcons.list_dash)),
+          IconButton(onPressed: () {}, icon: const Icon(CupertinoIcons.bell)),
+        ],
+        // PreferredSize - 자식의 레이아웃에 어떤 영향도 끼치지 않으면서 단지 부모에 의해 사용될 기본크기를 가지는 위젯
+        bottom: const PreferredSize(
+            child: Divider(
+              thickness: 0.5,
+              height: 0.5,
+              color: Colors.grey,
+            ),
+            preferredSize: Size.fromHeight(0.5)),
+      ),
+      body: Container(),
     );
   }
 }
