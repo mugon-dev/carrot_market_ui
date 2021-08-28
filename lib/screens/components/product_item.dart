82c53f5cdf736dcec13f724262f23a882f600c6d
mugon-dev <jmg41490400@gmail.com>
2021-08-28 18:44:35 +0900

    feat: add product list item ui

---
+++ lib/screens/components/product_item.dart
@@ -0,0 +1,29 @@
+import 'package:carrot_market_ui/models/product.dart';
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+  const ProductItem({required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 135.0,
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10.0),
+            child: Image.network(
+              product.urlToImage,
+              width: 115,
+              height: 115,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(width: 16.0),
+        ],
+      ),
+    );
+  }
+}
