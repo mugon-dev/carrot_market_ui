e46edaf552e3d14a85fdece1745e2e6c65785018
mugon-dev <jmg41490400@gmail.com>
2021-08-28 17:29:19 +0900

    feat: add indexedStack,bottomNavigation

--- lib/screens/main_screens.dart
+++ lib/screens/main_screens.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MainScreens extends StatefulWidget {
@@ -8,11 +9,50 @@ class MainScreens extends StatefulWidget {
 }
 
 class _MainScreensState extends State<MainScreens> {
+  int _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: const Text('MainScreen'),
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: [
+          Container(
+            color: Colors.orange[100],
+            child: Center(
+              child: Text(
+                'IndexedStack 1',
+                style: TextStyle(fontSize: 20, color: Colors.black),
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.redAccent[100],
+            child: Center(
+              child: Text(
+                'IndexedStack 2',
+                style: TextStyle(fontSize: 20, color: Colors.black),
+              ),
+            ),
+          )
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.chat_bubble),
+            label: '채팅',
+          )
+        ],
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        currentIndex: _selectedIndex,
       ),
     );
   }
