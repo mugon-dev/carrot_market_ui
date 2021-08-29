6c77e90a1cf8cebb83fbaeab561ccd213bff1763
mugon-dev <jmg41490400@gmail.com>
2021-08-29 15:52:26 +0900

    feat: publishing mycarrot header ui

--- lib/screens/components/my_carrot_header.dart
+++ lib/screens/components/my_carrot_header.dart
@@ -1,3 +1,4 @@
+import 'package:carrot_market_ui/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -32,14 +33,97 @@ class MyCarrotHeader extends StatelessWidget {
     );
   }
 
-  Container _buildRoundTextButton(String title, IconData iconData) {
-    return Container(color: Colors.orange[100], height: 60, width: 60);
+  Widget _buildRoundTextButton(String title, IconData iconData) {
+    return Column(
+      children: [
+        Container(
+          width: 60,
+          height: 60,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30.0),
+            color: Color.fromRGBO(255, 226, 208, 1),
+            border: Border.all(color: Color(0xffd4d5dd), width: 0.5),
+          ),
+          child: Icon(
+            iconData,
+            color: Colors.orange,
+          ),
+        ),
+        SizedBox(height: 10),
+        Text(
+          title,
+          style: textTheme().subtitle1,
+        )
+      ],
+    );
   }
 
-  Widget _buildProfileButton() =>
-      Container(color: Colors.blue[100], height: 45);
+  Widget _buildProfileButton() {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Color(0xFFD4D5DD),
+            width: 1.0,
+          ),
+          borderRadius: BorderRadius.circular(6.0),
+        ),
+        height: 45,
+        child: Center(
+          child: Text(
+            '프로필 보기',
+            style: textTheme().subtitle1,
+          ),
+        ),
+      ),
+    );
+  }
 
   Widget _buildProfileRow() {
-    return Container(color: Colors.redAccent[100], height: 60);
+    return Row(
+      children: [
+        Stack(
+          children: [
+            SizedBox(
+              width: 65,
+              height: 65,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(32.5),
+                child: Image.network(
+                  'https://placeimg.com/200/100/people',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child: Container(
+                width: 20,
+                height: 20,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.grey[100],
+                ),
+                child: Icon(
+                  Icons.camera_alt_outlined,
+                  size: 15,
+                ),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(width: 16),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('developer', style: textTheme().headline2),
+            SizedBox(height: 10),
+            Text('좌동 #00912'),
+          ],
+        ),
+      ],
+    );
   }
 }
