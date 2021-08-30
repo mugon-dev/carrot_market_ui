8e4c25b17ba1624f03509f6612a2d5ce54534393
mugon-dev <jmg41490400@gmail.com>
2021-08-30 19:03:24 +0900

    refactor: spread components

--- lib/screens/components/search_text_field.dart
+++ lib/screens/components/search_text_field.dart
@@ -15,10 +15,12 @@ class SearchTextField extends StatelessWidget {
           focusedBorder: _buildOutLineInputBorder(),
           filled: true,
           fillColor: Colors.grey[200],
+          // 글이 입력되는 content영역 앞에 icon 위젯을 사용
           prefixIcon: Icon(
             CupertinoIcons.search,
             color: Colors.grey,
           ),
+          // 글 입력 영역을 감싸고 있는 container의 padding 값
           contentPadding:
               const EdgeInsets.only(left: 0, bottom: 15, top: 15, right: 0),
           hintText: '좌동 주변 가게를 찾아 보세요',
@@ -30,7 +32,9 @@ class SearchTextField extends StatelessWidget {
 
   OutlineInputBorder _buildOutLineInputBorder() {
     return OutlineInputBorder(
+      // border의 색상과 두께
       borderSide: const BorderSide(width: 0.5, color: Color(0xffd4d5dd)),
+      // textfield 영역에 곡선
       borderRadius: BorderRadius.circular(8.0),
     );
   }
--- lib/screens/home/components/product_item.dart
+++ lib/screens/home/components/product_item.dart
@@ -1,5 +1,5 @@
 import 'package:carrot_market_ui/models/product.dart';
-import 'package:carrot_market_ui/screens/components/product_detail.dart';
+import 'package:carrot_market_ui/screens/home/components/product_detail.dart';
 import 'package:flutter/material.dart';
 
 class ProductItem extends StatelessWidget {
