6c34313badd90c86c4951ff8c37a3c2844bc7d29
mugon-dev <jmg41490400@gmail.com>
2021-08-30 18:29:37 +0900

    feat: add basic structure

--- lib/screens/near_me/near_me_screen.dart
+++ lib/screens/near_me/near_me_screen.dart
@@ -1,3 +1,5 @@
+import 'package:carrot_market_ui/screens/components/appbar_prefereed_size.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class NearMeScreen extends StatelessWidget {
@@ -5,8 +7,20 @@ class NearMeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('NearMeScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('내 근처'),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.pencil)),
+          IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.bell)),
+        ],
+        bottom: appBarBottomLine(),
+      ),
+      body: ListView(
+        children: [
+          const SizedBox(height: 10),
+        ],
+      ),
     );
   }
 }
